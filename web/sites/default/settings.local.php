8da23bcd62f022c7b1f5765b9f4a36c6c3d7c515
Kaushik Subramaniam <kaushik.subman@gmail.com>
2017-01-12 12:47:56 -0600

    templating

--- web/sites/default/settings.local.php
+++ web/sites/default/settings.local.php
@@ -109,5 +109,6 @@ $settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml'
 $settings['cache']['bins']['render'] = 'cache.backend.null';
 $settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
 $settings['trusted_host_patterns'] = array(
-  'igshpa.localhost'
+  'igshpa.localhost',
+  'igshpa2.localhost'
 );
